2dde345d0e149d7f938445ad4cf7617383868b76
vanomorodellefake <vladimirkrasutskiy2@mail.ru>
2025-09-15 13:38:44 +0500

    Lab 1 + practice

---
+++ Лабораторая работа 1/DataSystem.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лабораторая_работа_1
+{
+    public class DataSystem
+    {
+        private int _hour;
+        private int _minutes;
+        private int _seconds;
+        private int _secondsToAdd;
+
+        public int _Hour { get; set; }
+        public int _Minutes { get; set; }
+        public int _Seconds { get; set; }
+
+        private int _SecondsInHour = 3600;
+        private int _SecondsInMinute = 60;
+
+        private int _MinutesInHour = 60;
+        public int _SecondsToAdd { get; set; }
+
+        public DataSystem(int _hour, int _minutes, int _seconds, int _secondsToAdd)
+        {
+            _Hour = _hour;
+            _Minutes = _minutes;
+            _Seconds = _seconds;
+            _SecondsToAdd = _secondsToAdd;
+        }
+
+        public int ReturnSeconds(int hour, int minutes, int seconds)
+        {
+            return _SecondsInHour * hour + _SecondsInMinute * minutes + seconds;
+        }
+        
+        public (int, int, int) AddSomeSeconds(int hour, int minutes, int seconds, int secondstoadd)
+        {
+            int newHours = hour; int newMinutes = minutes; int newSeconds = seconds + secondstoadd;
+            if (newSeconds >= _SecondsInMinute)
+            {
+                while (newSeconds >= _SecondsInMinute)
+                {
+                    newMinutes++;
+                    newSeconds -= _SecondsInMinute;
+                }
+            }
+            if (newMinutes >= _MinutesInHour)
+            {
+                while (newMinutes >= _MinutesInHour)
+                {
+                    newHours++;
+                    newMinutes -= _MinutesInHour;
+                }
+            }
+
+            return (newHours, newMinutes, newSeconds);
+        }
+    }
+}
---
+++ Лабораторая работа 1/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Лабораторая_работа_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public DataSystem dataSystem { get; set; }
+
+        private int Hours;
+        private int Minutes;
+        private int Seconds;
+        private int SecondsToAdd;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ReturnSeconds(object sender, RoutedEventArgs e)
+        {
+            if (CheckZero())
+            {
+
+                dataSystem = new DataSystem(Hours, Minutes, Seconds, SecondsToAdd);
+                MessageBox.Show(Convert.ToString(dataSystem.ReturnSeconds(dataSystem._Hour, dataSystem._Minutes, dataSystem._Seconds)), "Количество секунд во времени");
+            } 
+        }
+
+        private void AddSomeSeconds(object sender, RoutedEventArgs e)
+        {
+            if (CheckZero())
+            {
+                dataSystem = new DataSystem(Hours, Minutes, Seconds, SecondsToAdd);
+                (int, int, int) NewTime = dataSystem.AddSomeSeconds(dataSystem._Hour, dataSystem._Minutes, dataSystem._Seconds, dataSystem._SecondsToAdd);
+
+                MessageBox.Show($"{NewTime.Item1} : {NewTime.Item2} : {NewTime.Item3}", "Время после добавления секунд");
+                //hours.Text = Convert.ToString(NewTime.Item1);
+                //minutes.Text = Convert.ToString(NewTime.Item2);
+                //seconds.Text = Convert.ToString(NewTime.Item3);
+            }
+        }
+        private void TextBox_Checker(object sender, KeyEventArgs e)
+        {
+            //e.Handled = true;
+            //MessageBox.Show("abc", "bca");
+            if (e.Key != Key.D0 && e.Key != Key.D1 && e.Key != Key.D2 && e.Key != Key.D3 && e.Key != Key.D4 && e.Key != Key.D5 && e.Key != Key.D6 && e.Key != Key.D7 && e.Key != Key.D8 && e.Key != Key.D9 && e.Key != Key.Back && e.Key != Key.Left && e.Key != Key.Right && e.Key != Key.Up && e.Key != Key.Down) 
+            {
+                //MessageBox.Show("abc", "bca");
+                e.Handled = true;
+            }
+        }
+        private bool CheckZero()
+        {
+            try
+            {
+                UpdateData();
+            }
+            catch
+            {
+                MessageBox.Show("Было найдено пустое поле", "Ошибка!");
+                return false;
+            }
+            return true;
+        }
+
+        private void UpdateData()
+        {
+            Hours = Convert.ToInt32(hours.Text);
+            Minutes = Convert.ToInt32(minutes.Text);
+            Seconds = Convert.ToInt32(seconds.Text);
+            SecondsToAdd = Convert.ToInt32(AmountSecondsToAdd.Text);
+        }
+
+        private void TextBox_CheckEntryTime(object sender, TextCompositionEventArgs e)
+        {
+            var Hours = Convert.ToInt32(hours.Text);
+            var Minutes = Convert.ToInt32(minutes.Text);
+            var Seconds = Convert.ToInt32(seconds.Text);
+            var SecondsToAdd = Convert.ToInt32(AmountSecondsToAdd.Text);
+
+            dataSystem = new DataSystem(Hours, Minutes, Seconds, SecondsToAdd);
+            var NewTime = dataSystem.AddSomeSeconds(dataSystem._Hour, dataSystem._Minutes, dataSystem._Seconds, 0);
+
+            hours.Text = Convert.ToString(NewTime.Item1);
+            minutes.Text = Convert.ToString(NewTime.Item2);
+            seconds.Text = Convert.ToString(NewTime.Item3);
+        }
+    }
+}
